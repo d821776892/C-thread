48d2af7b6957ea9add0f03c6da4969d8430c6f96
lab computer <dengyujiesc@163.com>
2020-06-18 21:03:45 +0800

    clion新版本

---
+++ cmake-build-debug/CMakeFiles/C_thread.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_thread.dir/main.cpp.o"
+  "C_thread"
+  "C_thread.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_thread.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
